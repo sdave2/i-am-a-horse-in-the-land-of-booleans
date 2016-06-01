a7b0566d202ac8893a94960793ba67d4550c5208
Shwetank Dave <sdave@Shwetanks-MacBook-Pro.local>
2016-06-01 16:36:43 -0400

    All tests pass!

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,48 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (case x
+    nil false
+    false false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (> x 0)
+    x
+    (* -1 x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (= 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (let [fizz ["" "" "fizz"]
+        buzz ["" "" "" "" "buzz"]
+        map-fn (fn [& a]
+                 (if (some clojure.string/blank? a)
+                   (apply str a)
+                   "gotcha!"))]
+    (->> (cycle buzz)
+         (map map-fn (cycle fizz))
+         (take n)
+         last)))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  ((complement teen?) age))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (-> x count (* 2))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond-> false
+    (= 0 (mod year 4)) not
+    (= 0 (mod year 100)) not
+    (= 0 (mod year 400)) not))
 
-; '_______'
+                                        ; '_______'
